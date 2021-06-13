d2ec225b2d756151280a76ada33f9e04c83d98b6
Kamyab Abedi <kamyababedi@gmail.com>
2021-06-13 13:36:06 +0200

    Register function about to finish

--- Project/controllers/authController.go
+++ Project/controllers/authController.go
@@ -2,6 +2,15 @@ package controllers
 
 import "github.com/gofiber/fiber/v2"
 
-func Hello(c *fiber.Ctx) error {
-	return c.SendString("Hello, World! ✌🏻")
+func Register(c *fiber.Ctx) error {
+	var data map[string]string
+
+	//err:=c.BodyParser(&data)
+	//if err != nil {
+	//	return err
+	//}
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+	return c.JSON(data)
 }
--- Project/database/connection.go
+++ Project/database/connection.go
@@ -3,12 +3,15 @@ package database
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"root/models"
 )
 
 func Connect() {
 	//db
-	_, err := gorm.Open(mysql.Open("root:rootroot@/snapfood_go_auth"), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open("root:rootroot@/snapfood_go_auth"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
+
+	db.AutoMigrate(&models.User{})
 }
