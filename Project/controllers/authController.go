bb2be75acb117299c9a26a5812bf7feb3eeac079
Kamyab Abedi <kamyababedi@gmail.com>
2021-06-14 20:11:21 +0200

    logout finished 🏃🏻‍♂️

--- Project/controllers/authController.go
+++ Project/controllers/authController.go
@@ -121,3 +121,19 @@ func User(c *fiber.Ctx) error {
 	return c.JSON(user)
 	//return c.JSON(claims)
 }
+
+func Logout(c *fiber.Ctx) error {
+	//just remove cookie
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
+}
