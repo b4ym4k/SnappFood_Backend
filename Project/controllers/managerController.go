703da072ae8d05d7ee6ef26f431e9f4889b2b495
Kamyab Abedi <kamyababedi@gmail.com>
2021-06-22 16:48:43 +0200

    still working...

--- Project/controllers/managerController.go
+++ Project/controllers/managerController.go
@@ -1,13 +1,51 @@
 package controllers
 
 import (
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
+	"root/database"
 	"root/models"
 )
 
 func ManagerCreateFood(c *fiber.Ctx) error {
-	var user models.User
-	return c.JSON(user)
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	//get cookie
+	cookie := c.Cookies("jwt")
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+
+	//user is not login
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "unauthenticated",
+		})
+	}
+
+	//claims to pointer of standard claims
+	claims := token.Claims.(*jwt.StandardClaims)
+
+	//var manager models.Manager
+
+	restaurant := models.Restaurant{
+		Food: []models.Food{
+			models.Food{
+				Name:  data["restaurantName"],
+				Price: data["price"],
+			},
+		},
+	}
+
+	database.DB.Where("id = ?", claims.Issuer).Model(&restaurant).Update("food", restaurant.Food)
+	database.DB.Create(&restaurant.Food)
+	return c.JSON(restaurant)
 }
 
 func ManagerDeleteFood(c *fiber.Ctx) error {
--- Project/models/food.go
+++ Project/models/food.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Food struct {
 	ID           uint   `gorm:"primaryKey" json:"id,omitempty"`
-	Name         string `gorm:"not null"json:"name,omitempty"`
-	Price        int    `gorm:"not null"json:"price,omitempty"`
+	Name         string `gorm:"not null" json:"name,omitempty"`
+	Price        string `gorm:"not null" json:"price,omitempty"`
 	Ready        bool   `json:"isReady,omitempty"`
 	Rate         int    `json:"rate,omitempty"`
 	RestaurantID int    `json:"restaurantID,omitempty"`
